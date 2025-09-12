b7cc5ecbe1dbbfc0aca57ac6169c5682b9cd8687
arnovr <arnovanrossum1@gmail.com>
2025-09-12 10:28:18 +0200

    First set up for the snapiter backend

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+	kotlin("jvm") version "2.2.20"
+	kotlin("plugin.spring") version "2.2.20"
+	id("org.springframework.boot") version "4.0.0-SNAPSHOT"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.snapiter"
+version = "0.0.1-SNAPSHOT"
+description = "SnapIter backend"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+	implementation("org.springframework.boot:spring-boot-starter-flyway")
+	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+	implementation("org.flywaydb:flyway-database-postgresql")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	implementation("org.springframework:spring-jdbc")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("org.postgresql:postgresql")
+	runtimeOnly("org.postgresql:r2dbc-postgresql")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.projectreactor:reactor-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
