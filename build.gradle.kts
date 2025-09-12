c0f502d05fa8868be4affc7ed419f8d423141ad6
arnovr <arnovanrossum1@gmail.com>
2025-09-12 10:36:43 +0200

    Add webflux webserver

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,9 +28,14 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 	implementation("org.springframework:spring-jdbc")
+
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
 	runtimeOnly("org.postgresql:postgresql")
 	runtimeOnly("org.postgresql:r2dbc-postgresql")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
