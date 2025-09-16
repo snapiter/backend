9591b2ecd5cf9aa8b4f1891590bd4419f9b914cd
arnovr <arnovanrossum1@gmail.com>
2025-09-16 15:23:21 +0200

    Disable flyway for this test, its not needed here

--- src/test/kotlin/com/snapiter/backend/BackendApplicationTests.kt
+++ src/test/kotlin/com/snapiter/backend/BackendApplicationTests.kt
@@ -2,10 +2,11 @@ package com.snapiter.backend
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
 
 @SpringBootTest
+@TestPropertySource(properties = ["spring.flyway.enabled=false"])
 class BackendApplicationTests {
-
 	@Test
 	fun contextLoads() {
 	}
