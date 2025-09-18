70e422d60c0b962ea0cf638fd0aa85e86688229d
arnovr <arnovanrossum1@gmail.com>
2025-09-18 16:13:44 +0200

    Proper check on the deviceId.

--- src/main/kotlin/com/snapiter/backend/security/DeviceAuthWebFilter.kt
+++ src/main/kotlin/com/snapiter/backend/security/DeviceAuthWebFilter.kt
@@ -2,6 +2,7 @@ package com.snapiter.backend.security
 
 import com.snapiter.backend.model.trackable.devices.DeviceRepository
 import com.snapiter.backend.model.trackable.devices.tokens.DeviceTokenService
+import com.snapiter.backend.model.trackable.devices.tokens.UnauthorizedTokenException
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.context.ReactiveSecurityContextHolder
@@ -23,10 +24,13 @@ class DeviceAuthWebFilter(
             .filter { it.deviceId == null } // only pass through active deviceIds
             .switchIfEmpty(Mono.error(UnauthorizedRefreshTokenException("device_token_invalid")))
             .flatMap { deviceToken ->
-                deviceRepo.findUserIdByDeviceId(deviceToken.deviceId!!) // custom query
+                val deviceId = deviceToken.deviceId
+                    ?: return@flatMap Mono.error(UnauthorizedTokenException("missing_device_id"))
+
+                deviceRepo.findUserIdByDeviceId(deviceId)
                     .map { userId ->
                         UsernamePasswordAuthenticationToken(
-                            DevicePrincipal(userId, deviceToken.deviceId),
+                            DevicePrincipal(userId, deviceId),
                             raw,
                             listOf(SimpleGrantedAuthority("ROLE_DEVICE"))
                         )
