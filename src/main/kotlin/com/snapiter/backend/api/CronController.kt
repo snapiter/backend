071d990d6ca8b705186e7cbd25418ec47be6d99f
arnovr <arnovanrossum1@gmail.com>
2025-09-23 14:10:44 +0200

    debug controller to run cron

---
+++ src/main/kotlin/com/snapiter/backend/api/CronController.kt
@@ -0,0 +1,17 @@
+package com.snapiter.backend.api
+
+import com.snapiter.backend.util.thumbnail.ThumbnailCron
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/public/cron")
+class CronController(
+    private val thumbnailCron: ThumbnailCron,
+) {
+    @GetMapping("/thumbnail")
+    fun runStaticMap() {
+        return thumbnailCron.cron()
+    }
+}
